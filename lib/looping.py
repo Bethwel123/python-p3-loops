d45fb4eeeb05693fa4e69209cd83e76ec9331774
Bethwel123 <bethwellangat654@gmail.com>
2024-08-27 15:30:14 +0300

    Used the control flow loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i ** 2 for i in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for number in range(1, 101):  # Loop from 1 to 100 inclusive
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
